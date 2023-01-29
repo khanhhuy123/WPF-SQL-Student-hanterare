66902601234a32cdf710e0b2eef5b9b13c22e451
khanhhuy123 <113548392+khanhhuy123@users.noreply.github.com>
2023-01-29 19:13:03 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_sql
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    /// 
+    public partial class MainWindow : Window
+    {
+        SQLiteHandler handler = new SQLiteHandler("myDB.db");
+        int doneNum;
+        bool check;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Hide visibility of adding student CheckBox and Label
+            firstNameLabel.Visibility = Visibility.Hidden;
+            firstTextBox.Visibility = Visibility.Hidden;
+
+            lastNameLabel.Visibility = Visibility.Hidden;
+            secondTextBox.Visibility = Visibility.Hidden;
+
+            ageLabel.Visibility = Visibility.Hidden;
+            thirdTextBox.Visibility = Visibility.Hidden;
+
+            IDLabel.Visibility = Visibility.Hidden;
+
+            CourseNameLabel.Visibility = Visibility.Hidden;
+
+            StudentID.Visibility = Visibility.Hidden;
+            CourseID.Visibility = Visibility.Hidden;
+
+            DoneBtn.Visibility = Visibility.Hidden;
+            cancelButton.Visibility = Visibility.Hidden;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton(object sender, RoutedEventArgs e)
+        {
+            //Hide visibility of adding student CheckBox and Label
+            firstNameLabel.Visibility = Visibility.Hidden;
+            firstTextBox.Visibility = Visibility.Hidden;
+
+            lastNameLabel.Visibility = Visibility.Hidden;
+            secondTextBox.Visibility = Visibility.Hidden;
+
+            ageLabel.Visibility = Visibility.Hidden;
+            thirdTextBox.Visibility = Visibility.Hidden;
+
+            IDLabel.Visibility = Visibility.Hidden;
+
+            DoneBtn.Visibility = Visibility.Hidden;
+            cancelButton.Visibility = Visibility.Hidden;
+        }
+
+        private void AddStudent(object sender, RoutedEventArgs e)
+        {
+            //Show visibility of adding student CheckBox and Label
+            firstNameLabel.Visibility = Visibility.Visible;
+            firstTextBox.Visibility = Visibility.Visible;
+
+            lastNameLabel.Visibility = Visibility.Visible;
+            secondTextBox.Visibility = Visibility.Visible;
+
+            ageLabel.Visibility = Visibility.Visible;
+            thirdTextBox.Visibility = Visibility.Visible;
+
+            DoneBtn.Visibility = Visibility.Visible;
+            cancelButton.Visibility = Visibility.Visible;
+            doneNum = 1;
+        }
+
+        private void RemoveStudentByID(object sender, RoutedEventArgs e)
+        {
+            IDLabel.Visibility = Visibility.Visible;
+            firstTextBox.Visibility = Visibility.Visible;
+            firstTextBox.Visibility = Visibility.Visible;
+
+            DoneBtn.Visibility = Visibility.Visible;
+            cancelButton.Visibility = Visibility.Visible;
+
+            doneNum = 2;
+        }
+        private void AddCourseBtn(object sender, RoutedEventArgs e)
+        {
+            CourseNameLabel.Visibility = Visibility.Visible;
+            firstTextBox.Visibility = Visibility.Visible;
+            DoneBtn.Visibility = Visibility.Visible;
+            cancelButton.Visibility = Visibility.Visible;
+            doneNum = 3;
+        }
+        private void AddStudentToCourse(object sender, RoutedEventArgs e)
+        {
+            StudentID.Visibility = Visibility.Visible;
+            CourseID.Visibility = Visibility.Visible;
+            firstTextBox.Visibility = Visibility.Visible;
+            secondTextBox.Visibility = Visibility.Visible;
+            DoneBtn.Visibility = Visibility.Visible;
+            cancelButton.Visibility = Visibility.Visible;
+            doneNum = 4;
+        }
+        private void FindStudentByFirstName(object sender, RoutedEventArgs e)
+        {
+            firstNameLabel.Visibility = Visibility.Visible;
+            firstTextBox.Visibility = Visibility.Visible;
+            DoneBtn.Visibility = Visibility.Visible;
+            cancelButton.Visibility = Visibility.Visible;
+            doneNum = 5;
+        }
+
+        private void DoneBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (doneNum == 1)
+            {
+                check = handler.AddStudentToStudentTable(firstTextBox.Text, secondTextBox.Text, Int32.Parse(thirdTextBox.Text));
+                if (check == true)
+                {
+                    MessageBox.Show("Data Added!");
+                    firstNameLabel.Visibility = Visibility.Hidden;
+                    firstTextBox.Visibility = Visibility.Hidden;
+
+                    lastNameLabel.Visibility = Visibility.Hidden;
+                    secondTextBox.Visibility = Visibility.Hidden;
+
+                    ageLabel.Visibility = Visibility.Hidden;
+                    thirdTextBox.Visibility = Visibility.Hidden;
+
+                    DoneBtn.Visibility = Visibility.Hidden;
+                    cancelButton.Visibility = Visibility.Hidden;
+                }
+            }
+            else if (doneNum == 2)
+            {
+                check = handler.RemoveStudentByID(Int32.Parse(firstTextBox.Text));
+                if (check == true)
+                {
+                    MessageBox.Show("Data Removed!");
+                    IDLabel.Visibility = Visibility.Hidden;
+                    firstTextBox.Visibility = Visibility.Hidden;
+
+                    DoneBtn.Visibility = Visibility.Hidden;
+                    cancelButton.Visibility = Visibility.Hidden;
+                }
+
+            }
+            else if (doneNum == 3)
+            {
+                check = handler.AddCourse(firstTextBox.Text);
+                if (check == true)
+                {
+                    MessageBox.Show("Data Added!");
+                    CourseNameLabel.Visibility = Visibility.Hidden;
+                    firstTextBox.Visibility = Visibility.Hidden;
+
+                    DoneBtn.Visibility = Visibility.Hidden;
+                    cancelButton.Visibility = Visibility.Hidden;
+                }
+            }
+            else if (doneNum == 4)
+            {
+                handler.AddStudentToCourse(Int32.Parse(firstTextBox.Text), Int32.Parse(secondTextBox.Text));
+                MessageBox.Show("Data Added!");
+                StudentID.Visibility = Visibility.Hidden;
+                CourseID.Visibility = Visibility.Hidden;
+                firstTextBox.Visibility = Visibility.Hidden;
+                secondTextBox.Visibility = Visibility.Hidden;
+
+                DoneBtn.Visibility = Visibility.Hidden;
+                cancelButton.Visibility = Visibility.Hidden;
+            }
+            else if (doneNum == 5)
+            {
+                handler.FindStudentByFirstName(firstTextBox.Text, dataGrid1);
+                firstNameLabel.Visibility = Visibility.Hidden;
+                firstTextBox.Visibility = Visibility.Hidden;
+
+                DoneBtn.Visibility = Visibility.Hidden;
+                cancelButton.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void ShowAllStudent(object sender, RoutedEventArgs e)
+        {
+            handler.ShowAllStudent(dataGrid1);
+        }
+
+        private void ShowAllStudentCourse(object sender, RoutedEventArgs e)
+        {
+            handler.ShowAllStudentCourse(dataGrid1);
+        }
+
+        private void ShowCourses(object sender, RoutedEventArgs e)
+        {
+            handler.ShowCourses(dataGrid1);
+        }
+    }
+}
---
+++ SQLiteHandler.cs
@@ -0,0 +1,286 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Windows.Controls;
+using System.Windows;
+using System.Data;
+
+namespace WPF_sql
+{
+    internal class SQLiteHandler
+    {
+        string? _database_name;
+        string _connection_string;
+        SQLiteConnection _connection; //kräver using System.Data.SQLite;
+        SQLiteCommand _command;
+        SQLiteDataAdapter dataAdp;
+        DataTable dt;
+
+        private void Open()
+        {
+            if (_connection != null)
+            {
+                _connection.Open();
+            }
+        }
+
+        private void Close()
+        {
+            if (_connection != null)
+            {
+                _connection.Close();
+            }
+        }
+
+        public SQLiteHandler(string databaseName)
+        {
+            _database_name = databaseName;
+            _connection_string = "URI=file:" + databaseName;
+            _connection = new SQLiteConnection(_connection_string);
+
+            CheckSQLiteVersion();
+            GetCurrentTime();
+        }
+
+        private void CheckSQLiteVersion()
+        {
+            Open();
+            string stm = "SELECT SQLITE_VERSION();";
+            _command = new SQLiteCommand(stm, _connection);
+            string? version = _command.ExecuteScalar().ToString();
+
+            Console.WriteLine($"SQLite version: {version}");
+            Close();
+        }
+
+        private void GetCurrentTime()
+        {
+            Open();
+            string stm = "SELECT(datetime('now', 'localtime'));";
+            _command = new SQLiteCommand(stm, _connection);
+            string? date = _command.ExecuteScalar().ToString();
+            Console.WriteLine($"SQLite dateNow: {date} \n");
+            Close();
+        }
+
+        //Skapa en table named student
+        public void CreateTable()
+        {
+            Open();
+            _command.CommandText = "CREATE TABLE students(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INT);";
+            _command.ExecuteNonQuery();
+
+            Console.WriteLine("Table 'students' created\n");
+            Close();
+        }
+
+        //Add student to student table
+        public bool AddStudentToStudentTable(string firstName, string lastName, int age)
+        {
+            Open();
+            _command.CommandText = "INSERT INTO students (firstName, lastName, age) VALUES (@firstName, @lastName, @age);";
+
+
+            SQLiteParameter firstNameParam = new SQLiteParameter("@firstName", System.Data.DbType.String);
+            SQLiteParameter lastNameParam = new SQLiteParameter("@lastName", System.Data.DbType.String);
+            SQLiteParameter ageParam = new SQLiteParameter("@age", System.Data.DbType.Int32);
+
+            firstNameParam.Value = firstName;
+            lastNameParam.Value = lastName;
+            ageParam.Value = age;
+
+            _command.Parameters.Add(firstNameParam);
+            _command.Parameters.Add(lastNameParam);
+            _command.Parameters.Add(ageParam);
+
+            _command.Prepare();
+            int check = _command.ExecuteNonQuery();
+            Close();
+
+            if (check == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Show all student in students table
+        public void ShowAllStudent(DataGrid dataGrid1)
+        {
+            Open();
+            try
+            {
+                _command.CommandText = "SELECT * FROM students;";
+                _command.ExecuteNonQuery();
+
+                dataAdp = new SQLiteDataAdapter(_command);
+                dt = new DataTable();
+                dataAdp.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+                Close();
+            }
+            catch (Exception expectation)
+            {
+                MessageBox.Show(expectation.Message);
+            }
+        }
+        //Show all courses and which student belong to the course
+        public void ShowAllStudentCourse(DataGrid dataGrid1)
+        {
+            Open();
+            try
+            {
+                _command.CommandText = "SELECT courses.courseName, students.firstName, students.lastName FROM students INNER JOIN courses ON courses.id = student_course.courseID INNER JOIN student_course ON students.id = student_course.studentID ORDER BY courseName ASC;";
+                _command.ExecuteNonQuery();
+
+                dataAdp = new SQLiteDataAdapter(_command);
+                dt = new DataTable();
+                dataAdp.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+                Close();
+            }
+            catch (Exception expectation)
+            {
+                MessageBox.Show(expectation.Message);
+            }
+        }
+
+        //Remove student using ID
+        public bool RemoveStudentByID(int id)
+        {
+            Open();
+            _command.CommandText = "DELETE FROM students WHERE id = @id;";
+
+            SQLiteParameter idParam = new SQLiteParameter("@id", System.Data.DbType.Int32);
+
+            idParam.Value = id;
+
+            _command.Parameters.Add(idParam);
+
+            _command.Prepare();
+            int check = _command.ExecuteNonQuery();
+
+            Close();
+
+            if (check == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Add course to courses table
+        public bool AddCourse(string courseName)
+        {
+            Open();
+            _command.CommandText = "INSERT INTO courses (courseName) VALUES (@courseName);";
+
+
+            SQLiteParameter courseNameParam = new SQLiteParameter("@courseName", System.Data.DbType.String);
+
+            courseNameParam.Value = courseName;
+
+            _command.Parameters.Add(courseNameParam);
+
+            _command.Prepare();
+            int check = _command.ExecuteNonQuery();
+            Close();
+
+            if (check == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Add studentID and courseID to student_course Table
+        public bool AddStudentToCourse(int studentID, int courseID)
+        {
+            Open();
+            _command.CommandText = "INSERT INTO student_course (studentID, courseID) VALUES (@studentID, @courseID);";
+
+            SQLiteParameter studentIDPARAM = new SQLiteParameter("@studentID", System.Data.DbType.Int32);
+            SQLiteParameter courseIDPARAM = new SQLiteParameter("@courseID", System.Data.DbType.Int32);
+
+            studentIDPARAM.Value = studentID;
+            courseIDPARAM.Value = courseID;
+
+            _command.Parameters.Add(studentIDPARAM);
+            _command.Parameters.Add(courseIDPARAM);
+
+
+            _command.Prepare();
+            int check = _command.ExecuteNonQuery();
+            Close();
+
+            if (check == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Show all course and courseID in courses table
+        public void ShowCourses(DataGrid dataGrid1)
+        {
+            Open();
+            try
+            {
+                _command.CommandText = "SELECT * FROM courses;";
+                _command.ExecuteNonQuery();
+
+                dataAdp = new SQLiteDataAdapter(_command);
+                dt = new DataTable();
+                dataAdp.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+                Close();
+            }
+            catch (Exception expectation)
+            {
+                MessageBox.Show(expectation.Message);
+            }
+        }
+
+        //Find Student by firstname
+        public void FindStudentByFirstName(string firstName, DataGrid dataGrid1)
+        {
+            Open();
+            try
+            {
+                _command.CommandText = "SELECT * FROM students WHERE firstName = @firstName;";
+                _command.Parameters.AddWithValue("@firstName", firstName);
+                _command.ExecuteNonQuery();
+
+                dataAdp = new SQLiteDataAdapter(_command);
+                dt = new DataTable();
+                dataAdp.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+                Close();
+            }
+            catch (Exception expectation)
+            {
+                MessageBox.Show(expectation.Message);
+            }
+
+        }
+    }
+
+}
+
